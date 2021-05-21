25cdcc4c2c47b7d3324b5986e3d4e9eacd4dd464
Natany Lima <naty.benzi@hotmail.com>
2021-05-21 11:49:51 -0300

    Adicionando pasta e arquivos referente pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
